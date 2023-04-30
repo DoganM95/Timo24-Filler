cab383b614e7b13c24edb6c6ae464e8a59b48e0c
DoganM95 <doganmermertas@hotmail.de>
2023-04-30 19:20:36 +0200

    finish concept

--- index.js
+++ index.js
@@ -5,14 +5,29 @@ const formatDayOrMonth = (month) => (String(month).length == 1 ? "0" + String(mo
 const dayNames = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
 
 (async () => {
+    // Prepare initial vars by current time
     const date = new Date();
     let currentMonth = date.getMonth() + 1;
     let currentYear = date.getFullYear();
     let firstDayOfCurrentMonth = "1";
     let lastDayOfCurrentMonth = daysInMonth(currentYear, currentMonth);
 
+    // Create array of dates for current month
     let daysOfCurrentMonthAsDates = [];
     for (let i = firstDayOfCurrentMonth; i <= lastDayOfCurrentMonth; i++) {
         daysOfCurrentMonthAsDates.push(new Date(String(currentYear) + "-" + formatDayOrMonth(currentMonth) + "-" + formatDayOrMonth(i)));
     }
+
+    // Filter out weekends
+    let workDaysOfCurrentMonth = [];
+    daysOfCurrentMonthAsDates.forEach((day) => {
+        if (dayNames[day.getDay()] != "saturday" && dayNames[day.getDay()] != "sunday") {
+            workDaysOfCurrentMonth.push(day);
+        }
+    });
+
+    // Fill timesheet workdays
+    workDaysOfCurrentMonth.forEach((day) => {
+        // console.log(day.toUTCString());
+    });
 })();
